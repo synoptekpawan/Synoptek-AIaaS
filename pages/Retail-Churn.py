7e773e2acb8b15bab7e7150bc9976a162c7336d1
synoptekpawan <99307097+synoptekpawan@users.noreply.github.com>
2023-01-27 19:36:32 +0530

    Update Retail-Churn.py

--- pages/Retail-Churn.py
+++ pages/Retail-Churn.py
@@ -7,27 +7,15 @@ import random
 np.random.seed(10)
 random.seed(10)
 import sys
-<<<<<<< HEAD
 sys.path.insert(0, r"./retailChurnAnalytics/")
-=======
-sys.path.insert(0, r".../../")
->>>>>>> 88dfedf678801cd959e6e50a20449b88128ef647
 from evaluateModelOnHoldData import evalModel
 from RetailChurnPrediction import RetailChurnPrediction
 from RetailChurnDashboard import RetailChurnDashboard
 
-<<<<<<< HEAD
 holdOuts = r"./retailChurnAnalytics/holdOutData/"
 outputs = r"./retailChurnAnalytics/outputs/"
 models = r"./retailChurnAnalytics/models/"
 logs = r"./retailChurnAnalytics/logs/"
-=======
-holdOuts = r".../../inputs"
-outputs = r".../../outputs/"
-models = r".../../models/"
-logs = r".../../logs/"
->>>>>>> 88dfedf678801cd959e6e50a20449b88128ef647
-
 
 def main ():
     try:
