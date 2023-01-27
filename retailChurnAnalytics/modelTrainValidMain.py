a2f6bb6cf214d6306d63e026b3f71ff88104678e
synoptekpawan <99307097+synoptekpawan@users.noreply.github.com>
2023-01-27 19:39:35 +0530

    Update modelTrainValidMain.py

--- retailChurnAnalytics/modelTrainValidMain.py
+++ retailChurnAnalytics/modelTrainValidMain.py
@@ -37,11 +37,7 @@ import os
 np.random.seed(10)
 random.seed(10)
 
-<<<<<<< HEAD
 sys.path.insert(0, r"./retailChurnAnalytics/utils/")
-=======
-sys.path.insert(0, r".../../utils/")
->>>>>>> 88dfedf678801cd959e6e50a20449b88128ef647
 from churnUtility import *
 from dataLabelingMain import dataLabelingMain
 from featureEnggMain import featureEnggMain
@@ -57,15 +53,9 @@ logging.basicConfig(filename='./logs/modelTrainValidMain.log',
 
 # -----------------------------------------------------------------------------------
 ## load date and folder variables
-<<<<<<< HEAD
 inputs = r"./retailChurnAnalytics/inputs/"
 outputs = r"./retailChurnAnalytics/outputs/"
 models = r"./retailChurnAnalytics/models/"
-=======
-inputs = r".../../inputs/"
-outputs = r".../../outputs/"
-models = r".../../models/"
->>>>>>> 88dfedf678801cd959e6e50a20449b88128ef647
 
 today_ = dt.datetime.today().date()
 # print(today_)
