e255f06a36ed4dafb4b5a2c33e539f6e24a12dce
synoptekpawan <99307097+synoptekpawan@users.noreply.github.com>
2023-01-27 18:35:07 +0530

    added relative paths

--- retailChurnAnalytics/evaluateModelOnHoldData.py
+++ retailChurnAnalytics/evaluateModelOnHoldData.py
@@ -33,7 +33,7 @@ import pickle
 np.random.seed(10)
 random.seed(10)
 
-sys.path.insert(0, r"D:/reatilanalytics/retailChurnAnalytics/utils/")
+sys.path.insert(0, r".../../utils/")
 from churnUtility import *
 from dataLabelingMain import dataLabelingMain
 from featureEnggMain import featureEnggMain
@@ -41,9 +41,9 @@ from featureSelectionMain import trainTestSplitWithBestFeatMain
  
 # -----------------------------------------------------------------------------------
 ## load date and folder variables
-holdOuts = r"D:/reatilanalytics/retailChurnAnalytics/holdOutData/"
-outputs = r"D:/reatilanalytics/retailChurnAnalytics/outputs/"
-models = r"D:/reatilanalytics/retailChurnAnalytics/models/"
+holdOuts = r".../../holdOutData/"
+outputs = r".../../outputs/"
+models = r".../../models/"
 
 today_ = dt.datetime.today().date()
 # print(today_)
