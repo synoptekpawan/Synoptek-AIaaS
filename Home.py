0701c1093d31e79d3c53074dcb7fd81b60196f7a
synoptekpawan <pchichghare@synoptek.com>
2023-09-08 11:40:40 +0530

    updated application

--- Home.py
+++ Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-
+# define page config
 st.set_page_config(
                     page_title = "Multipage App ",
                     page_icon = ":)",
                     layout="wide"
 
 )
-
+# set page info
 st.title("Synoptek - AIaaS")
 st.title("Welcome to Synoptek - AIaaS Service")
 st.sidebar.success("Select a page above")
